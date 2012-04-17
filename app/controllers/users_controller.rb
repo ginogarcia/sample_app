2f653e5ac23e1ad744a4924d2b0d89e485d83bfb
Gino Garcia <ginogarcia@netzero.net>
2012-04-16 19:50:01 -0500

    this one try

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,8 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       flash[:success] = "Welcome to the  Sample App!"
-      redirect_to user_path(@user)
-      # redirect_to @user
+     # redirect_to user_path(@user)
+      redirect_to @user
     else
       
       @title = "Sign up"
